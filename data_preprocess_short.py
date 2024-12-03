4d1171f7106f23d371e244f2e7bbfc9a7882b5a8
dali97 <ddiandian.li@gmail.com>
2024-12-03 04:16:10 -0800

    final changes

--- data_preprocess_short.py
+++ data_preprocess_short.py
@@ -4,13 +4,14 @@ import transformers as t
 # read the labeled_data.csv from t-davidson/hate-speech-and-offensive-language
 df = pd.read_csv('labeled_data.csv')
 
-# retrieve the first 400 rows and relevant columns
-df = df[['class', 'tweet']].head(400)
+# retrieve the first 250 rows of each class and relevant columns
+toxic = df[df['class'] != 2][['class', 'tweet']].head(250)
+nontoxic = df[df['class'] == 2][['class', 'tweet']].head(250)
+combined_df = pd.concat([toxic, nontoxic])
 
 # clean up and adjust the values of the columns
-df['tweet'] = df['tweet'].str.strip('!\' \"RT@')
-df['class'] = 2 - df['class']
-df['class'] = df['class'].apply(lambda x: 1 if x >= 1 else 0)
+combined_df['tweet'] = combined_df['tweet'].str.strip('!\' \"RT@')
+combined_df['class'] = combined_df['class'].apply(lambda x: 1 if x <= 1 else 0)
 
 # export data into csv file
-df.to_csv('cleaned_data_short.csv', index=False)
+combined_df.to_csv('cleaned_data_short.csv', index=False)
